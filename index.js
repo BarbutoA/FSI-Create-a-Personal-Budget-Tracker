334de64baa9af885eea0ef2998f908c1b37c3ddb
Your Name <anthonybarbuto2019@gmail.com>
2022-05-25 20:09:15 -0700

    starting my first save

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+console.log(weeklyExpensesQuestions)
+
+for(let i = 0; i < weeklyExpensesQuestions.length; i++){
+    let answer = window.prompt(weeklyExpensesQuestions[i]);
+    console.log(answer);
+    weeklyExpenses = weeklyExpenses + answer
+}
